7087aa04482a48c6d64ab1f5c823ffb07cdce5f9
abcuer <3486243105@qq.com>
2024-06-02 17:22:59 +0800

    练习配置并运行ROS2节点

---
+++ build/examples_rclcpp_cbg_executor/ament_cmake_symlink_install_targets_0_.cmake
@@ -0,0 +1,1 @@
+ament_cmake_symlink_install_targets("TARGET_FILES" "/home/nhmgs/d2lros2/build/examples_rclcpp_cbg_executor/ping" "/home/nhmgs/d2lros2/build/examples_rclcpp_cbg_executor/pong" "/home/nhmgs/d2lros2/build/examples_rclcpp_cbg_executor/ping_pong" "TARGETS" "ping" "pong" "ping_pong" "DESTINATION" "lib/examples_rclcpp_cbg_executor")
---
+++ chapt2/chapt2_ws/build/example_py/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/nhmgs/d2lros2/chapt2/chapt2_ws/install/example_py'
